b466fd1b30750e74c0c66c8df16ca585e3346f40
beyrerdn <beyrerdn@gmail.com>
2015-07-14 00:01:04 -0400

    Changes to Blackjack.rb and deck.rb. Comments included.

--- deck.rb
+++ deck.rb
@@ -4,12 +4,11 @@ class Deck
   attr_accessor :card
 
   def initialize
-    suit = %w(hearts, diamonds, clubs, spades)
+    suit = %w(hearts diamonds clubs spades) #removed commas because they were added to the suit strings for your card objects
     value = (2..10).to_a
     @card = []
     suit.each do |suit|
       value.each do |value|
-
         @card << Card.new(suit, value, value)
       end
     @card << Card.new(suit, 10, "J")
